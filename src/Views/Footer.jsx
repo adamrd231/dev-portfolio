a61849be21abbef3fdcd7f437ea9d3215276fb6a
Adam Reed <adam.rdconcepts@gmail.com>
2023-08-03 13:51:32 -0400

    Update footer

--- src/Views/Footer.jsx
+++ src/Views/Footer.jsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled'
-import { Paragraph } from '../Components/Typography';
 
 function Footer() {
 
@@ -22,8 +21,21 @@ function Footer() {
   `;
 
   const FooterItem = styled.div`
-    font-size: 1.5rem;
+    font-size: 1rem;
     font-weight: 600;
+    margin: 1rem 0 0.25rem 0;
+    text-transform: uppercase;
+    color: #555;
+    font-weight: 800;
+  `;
+  
+  const Paragraph = styled.div`
+    margin: 0.2rem 0;
+  `;
+
+  const Footnote = styled.div`
+    font-size: 0.75rem;
+    margin: 0.25rem 0;
   `;
 
   return (
@@ -34,13 +46,14 @@ function Footer() {
         <Paragraph>adam@rdconcepts.design</Paragraph>
       </FooterColumn>
       <FooterColumn>
-        <FooterItem>About Adam Reed</FooterItem>
+        <FooterItem>About this site</FooterItem>
         <Paragraph>
           Thank you for visiting my portfolio website and viewing my work.
           I built this website using React.js to develop the frontend portfolio and is deployed with firebase for easy hosting and ability to update with a GitHub Repository.
           iOS Designer & Developer, background in brand design and development.
           Located in Northern Michigan
         </Paragraph>
+        <Footnote>Updated Aug, 2023</Footnote>
       </FooterColumn>
 
       
